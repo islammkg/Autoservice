eb2f5977eb518c35ac5b43ec6f67ed9901efd292
Islam Modonov <modonov.islam@gmail.com>
2017-11-20 19:27:25 +0600

    jdbc is ready

---
+++ src/main/java/jdbc/entities/Main.java
@@ -0,0 +1,26 @@
+import entities.*;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        DataReader dataReader = new DataReader();
+
+        System.out.print("Input first name: ");
+        String firstName = scanner.nextLine();
+        System.out.print("Input last name: ");
+        String lastName = scanner.nextLine();
+        System.out.print("Input address: ");
+        String address = scanner.nextLine();
+        System.out.print("Input phone number: ");
+        String phoneNumber = scanner.nextLine();
+        System.out.println();
+
+        dataReader.insert(new Client(firstName, lastName, address, phoneNumber));
+
+        dataReader.readData();
+        dataReader.print();
+    }
+}
