efdb662eeb6589b119c9164111ad7b08a8374258
Islam Modonov <modonov.islam@gmail.com>
2017-11-20 21:32:18 +0600

    hibernate  tools

---
+++ src/main/java/hibernate/HibernateUtil.java
@@ -0,0 +1,20 @@
+package hibernate;
+
+import org.hibernate.cfg.Configuration;
+import org.hibernate.SessionFactory;
+
+public class HibernateUtil {
+    private static final SessionFactory sessionFactory;
+    static {
+        try {
+            sessionFactory = new Configuration().configure("/hibernate.cfg.xml").buildSessionFactory();
+        } catch (Throwable ex) {
+            System.err.println("Initial SessionFactory creation failed.» + ex" +ex.getMessage());
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
---
+++ src/main/java/jdbc/DataReader.java
@@ -0,0 +1,96 @@
+package jdbc;
+
+import jdbc.entities.*;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataReader {
+    private List<Address> addresses = new ArrayList<Address>();
+    private List<Auto> autos = new ArrayList<Auto>();
+    private List<Client> clients = new ArrayList<Client>();
+    private List<Employer> employers = new ArrayList<Employer>();
+
+    public DataReader () {}
+
+    public void readData() {
+        String url = "jdbc:mysql://localhost:3306/autoservice";
+        String user = "root";
+        String password = "";
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection(url, user, password);
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM client");
+            while (resultSet.next()) {
+                clients.add(new Client(resultSet.getString("first_name"),
+                                        resultSet.getString("last_name"),
+                                        resultSet.getString("address"),
+                                        resultSet.getString("phone_number")));
+            }
+        }
+        catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch (SQLException e) {
+            System.out.println("Unable to load driver class.");
+            e.printStackTrace();
+        }
+    }
+
+    public void read() {
+        String url = "jdbc:mysql://localhost:3306/autoservice";
+        String user = "root";
+        String password = "";
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection(url, user, password);
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM client");
+            while (resultSet.next()) {
+                clients.add(new Client(resultSet.getString("first_name"),
+                        resultSet.getString("last_name"),
+                        resultSet.getString("address"),
+                        resultSet.getString("phone_number")));
+            }
+        }
+        catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch (SQLException e) {
+            System.out.println("Unable to load driver class.");
+            e.printStackTrace();
+        }
+    }
+
+    public void insert(Client client) {
+        String url = "jdbc:mysql://localhost:3306/autoservice";
+        String user = "root";
+        String password = "";
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection(url, user, password);
+            Statement statement = connection.createStatement();
+            String query = "INSERT INTO client (first_name, last_name, address, phone_number) VALUES ('" + client.getFirstName() + "', '" + client.getLastName() +
+                    "', '" + client.getAddress() + "', '" + client.getPhoneNumber() + "')";
+            statement.executeUpdate(query);
+        }
+        catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch (SQLException e) {
+            System.out.println("Unable to load driver class.");
+            e.printStackTrace();
+        }
+    }
+
+    public void print() {
+        for (Client client: clients) {
+            System.out.println("Name: " + client.getFirstName() + " " + client.getLastName());
+            System.out.println("Address: " + client.getAddress());
+            System.out.println("Phone number: " + client.getPhoneNumber());
+            System.out.println();
+        }
+    }
+}
